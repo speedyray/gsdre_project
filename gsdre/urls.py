f48ab800151c67b4789dfe5f0d60b5750d35db4f
speedyray <speedyray2ray@gmail.com>
2019-11-07 15:01:06 -0500

    Add listing urls, templates and conditional highlighting

--- gsdre/urls.py
+++ gsdre/urls.py
@@ -1,12 +1,10 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+
