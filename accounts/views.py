24637c509d639dd38fb65df54944406b1dd0f2a4
speedyray <speedyray2ray@gmail.com>
2019-11-26 05:50:50 -0500

    Add registration, login and dynamic page links

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
@@ -13,11 +14,29 @@ def register(request):
 
         # Start validation here - Check if passwords match 
         if password == password2:
-            return
-
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    #Looks good, so create user 
+                    user = User.objects.create_user(username=username, password=password, email=email,
+                    first_name=first_name, last_name=last_name)
+                    # Login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
         else:
-            messages.error(request, 'Passwords do not match')
-            return redirect('register')     
+             messages.error(request, 'Passwords do not match')
+             return redirect('register')             
+                   
     else:
         return render(request, 'accounts/register.html' )
 
@@ -26,14 +45,31 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # Login user
-        return
+        # Login user by posting username and password
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate user using auth from django.contrib
+        user = auth.authenticate(username=username, password=password)
+        # Check if user is not None means user exists in database
+        if user is not None:
+            # use auth to login user
+            auth.login(request, user)
+            # post success messages
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            # post error messages if login fails
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html' )
    
 
 def logout(request):
-    return redirect('index' )
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index' )
 
 
 def dashboard(request):
